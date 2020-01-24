8f062f4f17eef360c07cbaa677d5b0df6c1c1bad
Kalyani <kalyanirane19@gmail.com>
2020-01-24 11:01:44 +0530

    ADDED : Function For Flipping A Coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,20 @@
 #!/bin/bash -x
 
+
 echo "Welcome to Flip Coin Simulator"
+
+#function for flip a coin
+function flipACoin()
+{
+	random=$((RANDOM % 2))
+
+	if [ $random -eq 0 ]
+	then
+			echo "Head"
+	else
+			echo "Tail"
+	fi
+}
+
+#Function Calling
+flipACoin
